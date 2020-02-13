ca0e391f9b84c7f2149ae5c6f20d4a55da96dcb6
panxogol <juan.martinez.p@usach.cl>
2020-02-13 02:08:18 -0300

    Inicio el codigo con un html y un js que contienen una app de dibujo con las flechas del teclado

---
+++ eventos.js
@@ -0,0 +1,73 @@
+function dibujarLinea(color, x_inicial, y_inicial, x_final, y_final, lienzo) {
+    //Aqui empieza el dibujo
+    lienzo.beginPath(); //funcion para poner el lapiz
+    lienzo.strokeStyle = color; //le da color a la linea, strokeStyle es una variable de lienzo
+    lienzo.lineWidth = 3; //se establece que el ancho de la linea es de 3 pixeles
+    lienzo.moveTo(x_inicial, y_inicial); //donde empezar a dibujar
+    lienzo.lineTo(x_final, y_final); //el tipo de dibujo es una linea hasta el pto 200,200
+    lienzo.stroke(); //Realizar el dibujo
+    lienzo.closePath(); //funcion para terminar el dibujo o trazo
+    //Si se desea crear un nuevo trazo, se debe realizar nuevamente un beginPath, en caso de que no se haga un cierre, se puede seguir dibujando pero a partir del punt en que se quedo la linea anterior.
+}
+
+//añadiremos una varieble que contiene constantes para asignar lo numeros 
+//representativos de las teclas del teclado a una palabra, constante (ESCRITAS EN MAYÚSCULAS, buenas practicas)
+//esta es la notación de json: javascript object notation
+var teclas = {
+    UP: 38,
+    DOWN: 40,
+    LEFT: 37,
+    RIGHT: 39
+}; //se pone punto y coma porque js lo interpreta como una sola linea de código
+
+document.addEventListener("keyup", dibujarTeclado);
+
+var cuadro = document.getElementById("area-de-dibujo");
+var pintura = cuadro.getContext("2d");
+var x = 150;
+var y = 150;
+var movimiento = 10;
+
+dibujarLinea("black", 149, 149, 151, 151, pintura);
+
+function dibujarTeclado(evento) {
+    if (x >= 0 && x <= 300 && y >= 0 && y <= 300) {
+        switch (evento.keyCode) { //keyCode es la variable que contiene el código numérico de la tecla segun el navegador
+            case teclas.UP:
+                if (y > 0) {
+                    dibujarLinea("blue", x, y, x, y - movimiento, pintura);
+                    y = y - movimiento;
+                    break;
+                } else {
+                    break;
+                }
+            case teclas.DOWN:
+                if (y < 300) {
+                    dibujarLinea("red", x, y, x, y + movimiento, pintura);
+                    y = y + movimiento;
+                    break;
+                } else {
+                    break;
+                }
+            case teclas.LEFT:
+                if (x > 0) {
+                    dibujarLinea("green", x, y, x - movimiento, y, pintura);
+                    x = x - movimiento;
+                    break;
+                } else {
+                    break;
+                }
+            case teclas.RIGHT:
+                if (x < 300) {
+                    dibujarLinea("yellow", x, y, x + movimiento, y, pintura);
+                    x = x + movimiento;
+                    break;
+                } else {
+                    break;
+                }
+            default:
+                console.log("otra tecla");
+                break; //no es estrictamente necesario este break
+        }
+    }
+}
